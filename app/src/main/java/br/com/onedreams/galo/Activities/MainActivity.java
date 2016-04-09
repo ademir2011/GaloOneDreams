cb9c7e55c23f8780931c6222fcfd1ca9104e5333
ademir <ademirbezerra2012@gmail.com>
2016-04-09 18:45:13 -0300

    teste2

--- app/src/main/java/br/com/onedreams/galo/Activities/MainActivity.java
+++ app/src/main/java/br/com/onedreams/galo/Activities/MainActivity.java
@@ -168,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
                                         }
                                     });
 
+
                                 }
 
                             }
